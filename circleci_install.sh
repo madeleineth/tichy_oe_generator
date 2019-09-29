8b415ce1d9c500895e90e7878b1283f7d7758019
Madeleine Thompson <madeleineth@gmail.com>
2019-09-29 15:51:51 -0400

    generate and upload artifacts on pushes to master

--- circleci_install.sh
+++ circleci_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential perl
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential bzip2 ca-certificates perl
 PERL_MM_USE_DEFAULT=1 cpan Perl::Tidy
